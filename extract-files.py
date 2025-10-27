c57defe29a6ca6631ce8954deecc3f0d973ca877
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-10-27 15:40:57 +0530

    fogo: Replace libui with libui-v34 in camera blobs

--- extract-files.py
+++ extract-files.py
@@ -32,6 +32,10 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/sensors.moto.so': blob_fixup()
         .add_needed('libbase_shim.so'),
+    ('vendor/lib64/camera/components/com.vidhance.node.ica.so',
+     'vendor/lib64/camera/components/com.vidhance.node.processing.so'
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),    
 }  # fmt: skip
 
 extract_fns: extract_fns_user_type = {
